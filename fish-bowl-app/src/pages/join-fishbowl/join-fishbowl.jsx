e456de4a7a3a643943df33e6e5d9a82c7a1ee581
TheGuzman <guzmanmedrano.diego@gmail.com>
2021-12-12 23:33:02 +0100

    Chat style added

--- fish-bowl-app/src/pages/join-fishbowl/join-fishbowl.jsx
+++ fish-bowl-app/src/pages/join-fishbowl/join-fishbowl.jsx
@@ -1,109 +1,137 @@
 import { useParams } from 'react-router'; import React, { useState, useEffect, useRef } from "react";
 import { styled } from '@mui/material/styles';
 import io from "socket.io-client"
+import { Box, palette } from '@mui/system';
+import { Button } from '@mui/material';
 
 const Page = styled('div')({
     display: 'flex',
     height: '100vh',
     width: '100%',
     alignItems: 'center',
-    backgroundColor: '#46516e',
+    backgroundColor: 'paper.main',
     flexDirection: 'column',
 })
 
-const Container = styled('div')({
+const Container = styled(Box)(({ theme }) => ({
     display: 'flex',
     flexDirection: 'column',
     height: '500px',
     maxHeight: '500px',
     overflow: 'auto',
-    width: '400px',
-    border: '1px solid lightgray',
+    width: '80%',
+    border: '2px solid',
+    borderColor: theme.palette.primary.main,
     borderRadius: '10px',
     paddingBottom: '10px',
     marginTop: '25px',
+}))
+const Form = styled('form')({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+    width: '80%',
 })
-const TextArea = styled('textarea')({
-    width: '98%',
-    height: '100px',
-    borderRadius: '10px',
-    marginTop: '10px',
+
+const TextArea = styled('textarea')(({ theme }) => ({
+    width: '80%',
+    height: '2em',
+    borderRadius: '4em',
+    marginTop:'0.5em',
     paddingLeft: '10px',
     paddingTop: '10px',
-    fontSize: '17px',
+    fontSize: '1.3rem',
     backgroundColor: 'transparent',
-    border: '1px solid lightgray',
+    border: '2px solid',
+    borderColor: theme.palette.primary.main,
     outline: 'none',
-    color: 'lightgray',
+    color: theme.palette.text,
     letterSpacing: '1px',
     lineHeight: '20px',
+}))
+
+const SendButton = styled(Button)(({ theme }) => ({
+    variant: 'contained',
+    width: '10%',
+    height: '2em',
+    marginTop:'0.5em',
+    borderRadius: '4em',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    fontSize: '1rem',
+}))
+
+
+const MyRow = styled('div')({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: '10px',
 })
 
+const MyMessage = styled('div')(({ theme }) => ({
+    width: 'fit-content',
+    backgroundColor: theme.palette.primary.light,
+    padding: '10px',
+    marginRight: '5px',
+    textAlign: 'center',
+    borderRadius: '10px',
+}))
 
-// const Button = styled.button`
-//   background-color: pink;
-//   width: 100%;
-//   border: none;
-//   height: 50px;
-//   border-radius: 10px;
-//   color: #46516e;
-//   font-size: 17px;
-// `;
+const SenderName = styled('p')(({ theme }) => ({
+    color: theme.palette.text,
+    margin: '0 0.2em',
+    textAlign: 'center',
+    fontSize: '0.7em',
+}))
 
-// const Form = styled.form`
-//   width: 400px;
-// `;
+const PartnerRow = styled(MyRow)({
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end'
+})
 
-// const MyRow = styled.div`
-//   width: 100%;
-//   display: flex;
-//   justify-content: flex-end;
-//   margin-top: 10px;
-// `;
-
-// const MyMessage = styled.div`
-//   width: 45%;
-//   background-color: pink;
-//   color: #46516e;
-//   padding: 10px;
-//   margin-right: 5px;
-//   text-align: center;
-//   border-top-right-radius: 10%;
-//   border-bottom-right-radius: 10%;
-// `;
-
-// const PartnerRow = styled(MyRow)`
-//   justify-content: flex-start;
-// `;
-
-// const PartnerMessage = styled.div`
-//   width: 45%;
-//   background-color: transparent;
-//   color: lightgray;
-//   border: 1px solid lightgray;
-//   padding: 10px;
-//   margin-left: 5px;
-//   text-align: center;
-//   border-top-left-radius: 10%;
-//   border-bottom-left-radius: 10%;
-// `;
+const PartnerMessage = styled('div')(({ theme }) => ({
+    width: 'fit-content',
+    backgroundColor: theme.palette.secondary.light,
+    color: theme.palette.text,
+    border: '1px solid lightgray',
+    padding: '10px',
+    marginLeft: '5px',
+    textAlign: 'center',
+    borderRadius: '10px',
+}))
 
 export default function JoinFishbowlPage() {
 
     const { roomId } = useParams()
-    const [name, setYourName] = useState(null)
+    // const [name, setYourName] = useState(null)
+    let [user, setUser] = useState(null)
     const [yourID, setYourID] = useState();
     const [room, setRoomID] = useState();
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
 
+
+    useEffect(() => {
+        fetch("http://localhost:3001/user", {
+            method: 'GET',
+            headers: {
+                "Authorization": sessionStorage.getItem('sesion')
+            }
+        })
+            .then(r => r.json())
+            .then(d => { setUser(d); console.log(d) })
+    }, [])
+
+
     const socketRef = useRef();
-   
+
     useEffect(() => {
         socketRef.current = io.connect('/');
 
         socketRef.current.emit('join-room', roomId);
-    
+
         console.log(socketRef.current)
 
         socketRef.current.on("your id", id => {
@@ -128,8 +156,8 @@ export default function JoinFishbowlPage() {
         const messageObject = {
             body: message,
             id: yourID,
-            name: name,
-            roomId:roomId,
+            name: user.name,
+            roomId: roomId,
         };
         setMessage("");
         socketRef.current.emit("send message", messageObject);
@@ -140,45 +168,35 @@ export default function JoinFishbowlPage() {
         setMessage(e.target.value);
     }
 
-    function handleUserName(e) {
-        e.preventDefault()
-        setYourName(e.target.name.value)
-    }
-
     return (
         <Page>
-            <form onSubmit={handleUserName}>
-                <input placeholder='your name' name='name'></input>
-                <button type='submit'>set name</button>
-            </form>
-            {name === null ? '' : <p>{name}</p>}
             <Container>
                 {messages.map((message, index) => {
                     if (message.id === yourID) {
                         return (
-                            <div key={index}>
-                                <div>
+                            <MyRow key={index}>
+                                <MyMessage>
                                     {message.body}
-                                </div>
-                            </div>
+                                </MyMessage>
+                            </MyRow>
                         )
                     }
                     return (
-                        <div key={index}>
-                            <div>
+                        <PartnerRow key={index}>
+                            <PartnerMessage>
                                 {message.body}
-                            </div>
-                            <div>
+                            </PartnerMessage>
+                            <SenderName>
                                 {message.name}
-                            </div>
-                        </div>
+                            </SenderName>
+                        </PartnerRow>
                     )
                 })}
             </Container>
-            <form onSubmit={sendMessage}>
-                <TextArea value={message} onChange={handleChange} placeholder="Say something..." />
-                <button>Send</button>
-            </form>
+                <Form onSubmit={sendMessage}>
+                    <TextArea value={message} onChange={handleChange} placeholder="Say something..." />
+                    <SendButton type='submit'>Send</SendButton>
+                </Form>
         </Page>
     );
 };
