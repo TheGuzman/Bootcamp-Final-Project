ebdb4e5f2ed3c6cd002dc5828c1746552c166840
TheGuzman <guzmanmedrano.diego@gmail.com>
2021-12-14 19:28:20 +0100

    Separated style chat components, created separate file for custom hook for streaming

---
+++ fish-bowl-app/src/components/styled-chat/styled-chat.jsx
@@ -0,0 +1,99 @@
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import { Button } from '@mui/material';
+
+export const Page = styled('div')({
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: 'paper.main',
+    flexDirection: 'column',
+})
+
+export const Container = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    height: '20em',
+    maxHeight: '20em',
+    overflow: 'auto',
+    width: '80%',
+    border: '2px solid',
+    borderColor: theme.palette.primary.main,
+    borderRadius: '10px',
+    paddingBottom: '10px',
+    marginTop: '25px',
+}))
+export const Form = styled('form')({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+})
+
+export const TextArea = styled('input')(({ theme }) => ({
+    width: '80%',
+    height: '2em',
+    borderRadius: '4em',
+    marginTop: '0.5em',
+    paddingLeft: '10px',
+    fontSize: '1.3rem',
+    backgroundColor: 'transparent',
+    border: '2px solid',
+    borderColor: theme.palette.primary.main,
+    outline: 'none',
+    color: theme.palette.text,
+    letterSpacing: '1px',
+}))
+
+export const SendButton = styled(Button)(({ theme }) => ({
+    variant: 'contained',
+    width: '10%',
+    height: '2em',
+    marginTop: '0.5em',
+    borderRadius: '4em',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    fontSize: '1rem',
+}))
+
+
+export const MyRow = styled('div')({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: '10px',
+})
+
+export const MyMessage = styled('div')(({ theme }) => ({
+    width: 'fit-content',
+    backgroundColor: theme.palette.primary.light,
+    padding: '10px',
+    marginRight: '5px',
+    textAlign: 'center',
+    borderRadius: '10px',
+}))
+
+export const SenderName = styled('p')(({ theme }) => ({
+    color: theme.palette.text,
+    margin: '0 0.2em',
+    textAlign: 'center',
+    fontSize: '0.7em',
+}))
+
+export const PartnerRow = styled(MyRow)({
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end'
+})
+
+export const PartnerMessage = styled('div')(({ theme }) => ({
+    width: 'fit-content',
+    backgroundColor: theme.palette.secondary.light,
+    color: theme.palette.text,
+    border: '1px solid lightgray',
+    padding: '10px',
+    marginLeft: '5px',
+    textAlign: 'center',
+    borderRadius: '10px',
+}))
--- fish-bowl-app/src/pages/join-fishbowl/join-fishbowl.jsx
+++ fish-bowl-app/src/pages/join-fishbowl/join-fishbowl.jsx
@@ -1,121 +1,23 @@
-import { useParams } from 'react-router'; import React, { useState, useEffect, useRef } from "react";
-import { styled } from '@mui/material/styles';
+import React from "react";
 import io from "socket.io-client"
-import { Box } from '@mui/system';
+import { useParams } from 'react-router';
+
 import { Stack } from '@mui/material';
-import { Button } from '@mui/material';
-import Peer from 'peerjs';
-import './join-fishbowl.css'
 
+import { Page, Container, MyRow, MyMessage, PartnerMessage, PartnerRow, SenderName, TextArea, SendButton, Form } from '../../components/styled-chat/styled-chat.jsx'
+import { useState, useRef, useEffect } from "react";
 
-const Page = styled('div')({
-    display: 'flex',
-    width: '100%',
-    alignItems: 'center',
-    backgroundColor: 'paper.main',
-    flexDirection: 'column',
-})
-
-const Container = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
-    height: '20em',
-    maxHeight: '20em',
-    overflow: 'auto',
-    width: '80%',
-    border: '2px solid',
-    borderColor: theme.palette.primary.main,
-    borderRadius: '10px',
-    paddingBottom: '10px',
-    marginTop: '25px',
-}))
-const Form = styled('form')({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '80%',
-})
-
-const TextArea = styled('input')(({ theme }) => ({
-    width: '80%',
-    height: '2em',
-    borderRadius: '4em',
-    marginTop: '0.5em',
-    paddingLeft: '10px',
-    fontSize: '1.3rem',
-    backgroundColor: 'transparent',
-    border: '2px solid',
-    borderColor: theme.palette.primary.main,
-    outline: 'none',
-    color: theme.palette.text,
-    letterSpacing: '1px',
-}))
-
-const SendButton = styled(Button)(({ theme }) => ({
-    variant: 'contained',
-    width: '10%',
-    height: '2em',
-    marginTop: '0.5em',
-    borderRadius: '4em',
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    fontSize: '1rem',
-}))
-
-
-const MyRow = styled('div')({
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginTop: '10px',
-})
-
-const MyMessage = styled('div')(({ theme }) => ({
-    width: 'fit-content',
-    backgroundColor: theme.palette.primary.light,
-    padding: '10px',
-    marginRight: '5px',
-    textAlign: 'center',
-    borderRadius: '10px',
-}))
-
-const SenderName = styled('p')(({ theme }) => ({
-    color: theme.palette.text,
-    margin: '0 0.2em',
-    textAlign: 'center',
-    fontSize: '0.7em',
-}))
-
-const PartnerRow = styled(MyRow)({
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end'
-})
-
-const PartnerMessage = styled('div')(({ theme }) => ({
-    width: 'fit-content',
-    backgroundColor: theme.palette.secondary.light,
-    color: theme.palette.text,
-    border: '1px solid lightgray',
-    padding: '10px',
-    marginLeft: '5px',
-    textAlign: 'center',
-    borderRadius: '10px',
-}))
 
 export default function JoinFishbowlPage() {
 
     const { roomId } = useParams()
-    // const [name, setYourName] = useState(null)
+    const socketRef = useRef();
+
+
     let [user, setUser] = useState(null)
     const [yourID, setYourID] = useState();
-    // const [allIDs, setAllIDs] = useState({});
-    // const [room, setRoomID] = useState();
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
-    const [streams, updateStream] = useState([]); 
-
 
     useEffect(() => {
         fetch("http://localhost:3001/user", {
@@ -129,71 +31,21 @@ export default function JoinFishbowlPage() {
     }, [])
 
 
-    const socketRef = useRef();
-
-    const peers = {}
-
-   
-    
-   
-    useEffect( () =>{
+    useEffect(() => {
         const connectRoom = async () => {
-            const myPeer = new Peer();
-        socketRef.current = io.connect('http://localhost:3001');
-        console.log(socketRef.current)
-        console.log('emitiendo join-room',roomId);
-        socketRef.current.emit('join-room', roomId);
-
-        socketRef.current.on("your id", id => {
-            setYourID(id);
-        })
-        socketRef.current.on("message", (message) => {
-            console.log('Llega un mensajitooooo',message)
-            receivedMessage(message);
-        })
-
-        await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        }).then(stream => {
-            addVideoStream(stream)
-            myPeer.on('call', call => {
-                call.answer(stream)
-                call.on('stream', userVideoStream => {
-                    addVideoStream(userVideoStream)
-                })
+            socketRef.current = io.connect('http://localhost:3001');
+            socketRef.current.emit('join-room', roomId);
+            socketRef.current.on("userId", id => {
+                setYourID(id);
+                console.log(id)
             })
-            function connectToNewUser(userId, stream) {
-                const call = myPeer.call(userId, stream);
-                let newUserStream;
-                call.on('stream', userVideoStream => {
-                    newUserStream = userVideoStream;
-                    addVideoStream(userVideoStream)
-                })
-                call.on('close', () => {
-                    // video.remove()
-                    const i = streams.findIndex( s => s === newUserStream);
-                    streams.splice(i,1);
-                    updateStream([...streams]);
-                })
-        
-                peers[userId] = call
-            }
-            socketRef.current.on('user-connected', userId => {
-                connectToNewUser(userId, stream)
-                console.log(userId)
+            socketRef.current.on("message", (message) => {
+                receivedMessage(message);
             })
-        })
- 
-        socketRef.current.on('user-disconnected', userId => {
-            if (peers[userId]) peers[userId].close()
-        })
-        myPeer.on('open', id => {
-            // socketRef.current.emit('join-room', roomId, id)
-        })
-    }
-    connectRoom();
+        }
+        connectRoom();
     }, []);
+
 
     function receivedMessage(message) {
         setMessages(oldMsgs => [...oldMsgs, message]);
@@ -205,7 +57,6 @@ export default function JoinFishbowlPage() {
             body: message,
             id: yourID,
             name: user.name,
-            roomId: roomId,
         };
         setMessage("");
         socketRef.current.emit("send message", messageObject);
@@ -220,28 +71,12 @@ export default function JoinFishbowlPage() {
         }
     };
 
-    
-
-function addVideoStream(stream) {
-        // video.srcObject = stream
-        // video.addEventListener('loadedmetadata', () => {
-        //     video.play()
-        // })
-        // const videoGrid = document.getElementById('video-grid')
-        // videoGrid.append(video)
-        streams.push(stream);
-        updateStream([...streams])
-    }
-
-
-
-
     return (
         <Stack>
             <Stack>
-                <div id="video-grid">
-                    {streams.map((s,i) => <Video key={i} stream={s} />)}
-                </div>
+                {/* <div id="video-grid">
+    // //                 {streams.map((s, i) => <Video key={i} stream={s} />)}
+    // //             </div> */}
             </Stack>
             <Page>
                 <Container>
@@ -273,16 +108,6 @@ function addVideoStream(stream) {
                 </Form>
             </Page>
         </Stack>
-
-    );
+    )
 };
 
-
-const Video = ({ stream }) => {
-    const localVideo = React.createRef();
-    useEffect(() => {
-      if (localVideo.current) localVideo.current.srcObject = stream;
-    }, [stream, localVideo]);
-  
-    return <video style={{ height: 200, width: 200 }} ref={localVideo} autoPlay />
-  };
