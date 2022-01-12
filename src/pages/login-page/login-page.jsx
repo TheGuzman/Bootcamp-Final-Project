9c709a43b506b1f006196c32c89d42b47344246e
TheGuzman <guzmanmedrano.diego@gmail.com>
2022-01-12 18:51:19 +0100

    test login heroku

--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -61,7 +61,7 @@ export default function LoginPage() {
                     userEmail: userEmail,
                 }),
             };
-            fetch(`${url}/auth/login`, options)
+            fetch(`https://fishbowl-backend-app.herokuapp.com/auth/login`, options)
                 .then(r => r.json())
                 .then(d => {
                     console.log(d)
